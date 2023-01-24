0a638945151f6286ff5d3316bdfbe1343b81eaf7
Brandon Awan <brandon@axumtechnologies.com>
2023-01-24 17:20:55 +0100

    updates

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
 from . models import Post
+from .forms import PostForm
 
 # posts = Post.objects.all()
 
@@ -12,3 +14,30 @@ def post_list(request):
 def post_detail(request, id):
     posts = Post.objects.get(id=id)
     return render(request,'base/post_detail.html', {'posts': posts})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'base/post_edit.html', {'form': form})
+
+def post_edit(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'base/post_edit.html', {'form': form})
